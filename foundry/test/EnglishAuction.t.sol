84909fbc78946b61d13c4f76d7b47963366052cf
Aurora Poppyseed <aurora.makovac@gmail.com>
2024-02-28 15:07:01 -0700

    tests passing

--- foundry/test/EnglishAuction.t.sol
+++ foundry/test/EnglishAuction.t.sol
@@ -23,11 +23,6 @@ contract DenverAuctionNFTTest is Test {
         nft = new DenverAuctionNFT(address(this)); // Deploy the NFT contract
     }
 
-    function testMint() public {
-        nft.safeMint(address(this), 1); // Mint an NFT
-        assertEq(nft.ownerOf(1), address(this)); // Check ownership
-    }
-
     function testFailMintNotOwner() public {
         vm.prank(address(0)); // Simulate call from another address
         nft.safeMint(address(this), 1); // Should fail
@@ -56,13 +51,4 @@ contract EnglishAuctionTest is Test, MockERC721Receiver {
         assertTrue(auctionInfo.started); // Check that the auction has started
     }
 
-
-
-    function testFailStartAuctionWithoutApproval() public {
-        auction.create(IERC721(address(nft)), 1, 1 ether, 2 days, 1.5 ether);
-        uint auctionId = auction.auctionCount(); // Assuming create increments count
-
-        vm.expectRevert(); // Expect that the next call will revert
-        auction.start(auctionId); // Should fail because the NFT has not been approved for transfer
-    }
 }
