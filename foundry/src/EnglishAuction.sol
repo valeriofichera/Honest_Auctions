9790bd36060794f76a03405aa491cf0d97744082
Aurora Poppyseed <aurora.makovac@gmail.com>
2024-02-28 15:03:01 -0700

    added tests

--- foundry/src/EnglishAuction.sol
+++ foundry/src/EnglishAuction.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-interface IERC721 {
-    function safeTransferFrom(address from, address to, uint tokenId) external;
+// interface IERC721 {
+//     function safeTransferFrom(address from, address to, uint tokenId) external;
 
-    function transferFrom(address, address, uint) external;
-}
+//     function transferFrom(address, address, uint) external;
+// }
+
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
 
 contract EnglishAuction {
     struct Auction {
