d010ee6e03ba80691b3c8ddf7d5c775e07b3dcf4
Valerio <78494380+valeriofichera@users.noreply.github.com>
2024-02-27 19:36:28 -0700

    update

--- foundry/script/DenverAuctionNFT.s.sol
+++ foundry/script/DenverAuctionNFT.s.sol
@@ -4,9 +4,15 @@ pragma solidity ^0.8.19;
 import {Script, console} from "forge-std/Script.sol";
 
 contract DenverAuctionNFT is Script {
-    function setUp() public {}
+
+    address;
 
     function run() public {
+        vm.envUnit("PRIVATE_KEY");
         vm.broadcast();
+        function safeMint(address to, uint256 tokenId) public onlyOwner {
+            _safeMint(to, tokenId);
+        }
+
     }
 }
