e9cd63eb97bbb29db30aedaa27eacac5b9d334ed
Aurora Poppyseed <aurora.makovac@gmail.com>
2024-02-28 21:01:35 -0700

    generating abis

--- foundry/deploy.sh
+++ foundry/deploy.sh
@@ -23,11 +23,13 @@ fi
 # Deploy DenverAuctionNFT
 echo "Deploying DenverAuctionNFT..."
 # forge script ./script/DeployDenverAuctionNFT --broadcast --rpc-url $RPC_URL --private-key $PRIVATE_KEY -vvv
-forge create --rpc-url $RPC_URL --private-key $PRIVATE_KEY src/DenverAuctionNFT.sol:DenverAuctionNFT --constructor-args $PUBLIC_ADDRESS
+# forge create --rpc-url $RPC_URL --private-key $PRIVATE_KEY --constructor-args $PUBLIC_ADDRESS --etherscan-api-key $ETHERSCAN_API_KEY --verify src/DenverAuctionNFT.sol:DenverAuctionNFT
+forge script script/DenverAuctionNFT.s.sol:DeployDenverAuctionNFT --fork-url $RPC_URL --private-key $PRIVATE_KEY --broadcast --etherscan-api-key $ETHERSCAN_API_KEY --verify -vvv
 
 # Deploy EnglishAuction
 echo "Deploying EnglishAuction..."
 # forge script ./script/DeployEnglishAuction --broadcast --rpc-url $RPC_URL --private-key $PRIVATE_KEY -vvv
-forge create --rpc-url $RPC_URL --private-key $PRIVATE_KEY src/EnglishAuction.sol:EnglishAuction
+# forge create --rpc-url $RPC_URL --private-key $PRIVATE_KEY --etherscan-api-key $ETHERSCAN_API_KEY --verify src/EnglishAuction.sol:EnglishAuction
+forge script script/EnglishAuction.s.sol:DeployEnglishAuction --fork-url $RPC_URL --private-key $PRIVATE_KEY --broadcast --etherscan-api-key $ETHERSCAN_API_KEY --verify -vvv
 
 echo "Deployment completed."
