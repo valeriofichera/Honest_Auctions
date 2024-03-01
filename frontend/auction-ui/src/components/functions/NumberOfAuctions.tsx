b33e81f21c757c2bb277745399686039855bc0b2
Aurora Poppyseed <aurora.makovac@gmail.com>
2024-03-01 07:26:00 -0700

    runs

---
+++ frontend/auction-ui/src/components/functions/NumberOfAuctions.tsx
@@ -0,0 +1,29 @@
+import { useContractRead } from 'wagmi';
+import EnglishAuctionABI from '../../../constants/abi/EnglishAuction';
+import { ENGLISH_AUCTION_ADDRESS_SEPOLIA } from '../../../constants/deployed_address.ts';
+
+function NumberOfAuctions() {
+  const { data, isError, isLoading, error} = useContractRead({
+    address: ENGLISH_AUCTION_ADDRESS_SEPOLIA,
+    abi: EnglishAuctionABI, // Assuming reading from English Auction contract, adjust as necessary
+    functionName: 'getHunger',
+    args: [],
+    watch: true, // Re-run the function when args change
+});
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error?.message}</div>;
+  }
+
+  return (
+    <div>
+      Number of Auctions: {data?.toString()} {/* Assuming 'data' will be the number of auctions */}
+    </div>
+  );
+}
+
+export default NumberOfAuctions;
