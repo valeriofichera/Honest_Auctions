5787284af3ddc7f33ba3f6b64fd010dea8dae301
Valerio <78494380+valeriofichera@users.noreply.github.com>
2024-03-02 02:25:30 -0700

    added to logoslider

--- frontend/auction-ui/src/components/LogoSlider.tsx
+++ frontend/auction-ui/src/components/LogoSlider.tsx
@@ -1,12 +1,20 @@
 
 import arb from '../assets/Arbitrum.svg';
 import fuel from '../assets/Fuel.svg'
+import base from '../assets/base.svg'
+import threshold from '../assets/threshold.svg'
+import linea from '../assets/linea.svg'
+import moonbeam from '../assets/moonbeam.svg'
+import fhenix from '../assets/fhenix.svg'
+import artela from '../assets/artela.svg'
+
+
 
 
 import '../styles/Animations.css';
 
 function LogoSlider() {
-  const logos = [fuel, arb,fuel, arb,fuel, arb,fuel, arb,fuel, arb,fuel, arb,fuel, arb,fuel, arb,fuel, arb,fuel, arb,fuel, arb,]
+  const logos = [fuel, arb, base, threshold, linea, moonbeam, fhenix, artela, fuel, arb, base, threshold, linea, moonbeam, fhenix, artela, fuel, arb, base, threshold, linea, moonbeam, fhenix, artela]
   return (
     <div className="mt-[-67px] w-screen overflow-hidden bg-gradient-to-r from-[#0B0C15] via-[#2B4D7E]/50 to-[#0B0C15] py-5 mb-12">
       {/* Use flex and items-center to vertically center the logos */}
